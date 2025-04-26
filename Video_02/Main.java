8161da5ff21bb35e9bd38e10db1a33a62bd956e0
Satyam-Shinde <satyamshinde2004@gmail.com>
2025-04-26 22:09:29 +0530

    Video_01 and Video_02 code uploaded

---
+++ Video_02/Main.java
@@ -0,0 +1,25 @@
+package Video_02;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+//        1. output
+//        System.out.println("*\n**\n***\n****");
+
+
+//        2. Variables
+//        String name = "Satyam Shinde";
+//        System.out.println(name);}
+
+//        3. Quiz
+//        int a = 10;
+//        int b = 5;
+//        int ans = (a * b) / (a - b);
+//        System.out.println(ans);
+
+//        4. Inputs
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        System.out.println(a+b);
+    }
+}
---
+++ Video_02/Test.java
@@ -0,0 +1,33 @@
+package Video_02;
+
+public class Test {
+    public static void main(String[] args){
+//        Q.1 : Write a program to print the following pattern using System.out.println():
+//        1
+//        12
+//        123
+//        1234
+//        System.out.println("1\n12\n123\n1234");
+
+//        Q.2 : Print your name, age, and your favorite quote using System.out.println().
+//        System.out.println("My name is Satyam Shinde");
+//        System.out.println("My age is 20 years");
+//        System.out.println("Hardest choices required the strongest will");
+
+//        Q.3 : Declare variables for a student's name, roll number, and marks in three subjects. Print their total marks.
+//        String name = "Student 1";
+//        int roll = 03;
+//        int sub1 = 12;
+//        int sub2 = 16;
+//        int sub3 = 18;
+//        int sum = (sub1+sub2+sub3);
+//        System.out.println("Name : "+ name +", Roll: "+ roll+ " Total Marks: "+ sum);
+
+//        Q.4 : Create variables to store the radius of a circle. Calculate and print the area of the circle (area = π * r * r, use 3.14 for π).
+        int radius = 7;
+        double pi = 3.14;
+        double area = (pi*radius*radius);
+        System.out.println("Area : "+ area);
+    }
+
+}

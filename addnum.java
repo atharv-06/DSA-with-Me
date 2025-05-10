8dac354c8aef8d064754d0c1dd9d5bd7b0b8d573
atharv <lokhandeatharva225@gmail.com>
2025-05-10 23:33:14 +0530

    simple addition program

---
+++ addnum.java
@@ -0,0 +1,12 @@
+
+
+public class addnum{
+    public static void main(String[] args) {
+        int num_1=20;
+        int num_2=38;
+
+        int sum = num_1 + num_2;
+
+        System.out.println("The Sum Of Two Number is :" + sum);
+    }
+}
